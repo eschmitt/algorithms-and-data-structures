de05aa31c2d345c2fb274055046bce41bd819bcb
Eric Schmitt <ericschmitt1@gmail.com>
2016-06-02 12:35:59 -0400

    Remove test helper from package file test script. Add trees test file with initial existence test for BinaryHeap.

---
+++ test/trees.js
@@ -0,0 +1,8 @@
+import {assert} from 'chai';
+import {BinaryHeap} from '../datastructures/trees';
+
+describe('trees', () => {
+  it('should exist as a function', () => {
+    assert.isFunction(BinaryHeap);
+  });
+});
